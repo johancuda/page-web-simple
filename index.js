4a3beee8567a81236f0d1cb4510782e89cca8bfd
johancuda <61778212+johancuda@users.noreply.github.com>
2023-09-24 17:12:28 +0200

    added interaction with span

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+let texteQuiTourne = document.createElement('span')
+texteQuiTourne.innerHTML = "Je n'arrive pas à décider ma couleur du jour (Passez votre curseur sur moi)"
+texteQuiTourne.addEventListener('mouseover', (e) => {
+    let random = Math.floor(Math.random()*360)
+    let random_percentage = Math.floor(Math.random()*100)
+    console.log(random)
+    e.target.style.color = `hsl(${random}, ${random_percentage}%, ${random_percentage}%)`
+})
+document.body.append(texteQuiTourne)
+
+
+
+
 // Cette fonction s'exécute lorsque l'utilisateur.ice clique sur le bouton
 
 function myfunction() {
